e50c85ec8159744cd943b70ffdde3898f9a33b50
Noah Waldner <git@noahwaldner.ch>
2021-10-25 21:15:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import boxen from 'boxen'
+import chalk from 'chalk'
+
+const y = chalk.yellow;
+const g = chalk.green;
+const c = chalk.cyan;
+const b = chalk.bold;
+const o = chalk.rgb(237,125,62)
+
+const cardOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "#e28743"
+};
+
+const content = `                  ${b("Noah Waldner")}
+
+${g("Job")}                        ${o("Frontend Engineer at Valtech")}
+
+${g("Twitter")}                    ${y("https://twitter.com/noahwaldner")}
+${g("GitHub")}                     ${y("https://github.com/noahwaldner")}
+${g("Website")}                    ${y("https://waldner.dev")}
+${g("Instagram")}                  ${y("https://instagram.com/swiss_fries")}
+${g("LinkedIn")}                   ${y("https://linkedin.com/in/noahwaldner/")}
+
+                    ${b("Featured Projects")}
+
+${g("Basler Kantonalbank")}        ${y("https://bkb.ch")}
+${g("Adobe Event Website")}        ${y("https://adobe.com")}
+${g("Valyo")}                      ${y("https://valyo.ch")}
+${g("888 Productions")}            ${y("https://888productions.studio")}
+`;
+
+const slidesContent = ``;
+
+const card = boxen(content, cardOptions);
+
+console.log(card);

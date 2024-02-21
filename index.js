b20b872b6c75896313793c4d442e00948c93551a
Noah Waldner <noah.waldner@frontify.com>
2024-02-21 18:07:38 +0100

    update bio

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
 
-import boxen from 'boxen'
-import chalk from 'chalk'
+import boxen from "boxen";
+import chalk from "chalk";
 
 const y = chalk.yellow;
 const g = chalk.green;
 const c = chalk.cyan;
 const b = chalk.bold;
-const o = chalk.rgb(237,125,62)
+const o = chalk.rgb(237, 125, 62);
 
 const cardOptions = {
-    padding: 1,
-    margin: 1,
-    borderStyle: "round",
-    borderColor: "#e28743"
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "#e28743",
 };
 
 const content = `                  ${b("Noah Waldner")}
 
-${g("Job")}                        ${o(
-  "Frontend Engineer at Digitec Galaxus AG"
-)}
+${g("Job")}                        ${o("Frontend Tech Lead at Frontify")}
 
 ${g("Twitter")}                    ${y("https://twitter.com/noah_waldner")}
 ${g("GitHub")}                     ${y("https://github.com/noahwaldner")}
@@ -31,7 +29,9 @@ ${g("LinkedIn")}                   ${y("https://linkedin.com/in/noahwaldner/")}
 
                     ${b("Featured Projects")}
 
-${g("Digiteg Galaxus")}            ${y("https://galaxus.ch")}
+${g("HNY Tech")}                   ${y("https://hny.tech")}
+${g("Frontify")}                   ${y("https://frontify.com")}
+${g("Digitec Galaxus")}            ${y("https://galaxus.ch")}
 ${g("Adobe Event Website")}        ${y("https://adobe.com")}
 ${g("Basler Kantonalbank")}        ${y("https://bkb.ch")}
 ${g("Axpo")}                       ${y("https://axpo.com")}
